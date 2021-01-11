09299241b3299642796dc46d1197ce2a7e7530ff
Glippy <jonathanong@iMac.local>
2021-01-11 10:55:00 +0800

    PCMOB05 submission

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -1,16 +1,57 @@
-import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
-import { commonStyles } from "../styles/commonStyles";
+import React, { useState, useEffect } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { commonStyles } from '../styles/commonStyles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+
+const API = 'https://ongaikngee.pythonanywhere.com';
+const API_WHOAMI = '/whoami';
 
 export default function AccountScreen({ navigation }) {
-  function signOut() {}
+	const [ username, setUsername ] = useState('');
 
-  return (
-    <View style={commonStyles.container}>
-      <Text>Account Screen</Text>
-      <Button title="Sign out" onPress={signOut} />
-    </View>
-  );
+	async function getUsername() {
+
+    //get my token
+    //send it to the API and save the user name
+    //if succeed, we will display the user name, 
+    //if error....do nothing
+		console.log('---- Getting user name ----');
+		const token = await AsyncStorage.getItem('token');
+		console.log(`Token is ${token}`);
+		try {
+			const response = await axios.get(API + API_WHOAMI, {
+				headers: { Authorization: `JWT ${token}` }
+			});
+			console.log('Got user name!');
+			console.log(response);
+			setUsername(response.data.username);
+		} catch (error) {
+			console.log('Error getting user name');
+			if (error.response) {
+				console.log(error.response.data);
+			} else {
+				console.log(error);
+			}
+		}
+	}
+
+	useEffect(() => {
+		getUsername();
+	}, []);
+
+	function signOut() {
+		AsyncStorage.removeItem('token');
+		navigation.navigate('SignIn');
+	}
+
+	return (
+		<View style={commonStyles.container}>
+			<Text>Account Screen</Text>
+			<Text>{username}</Text>
+			<Button title="Sign out" onPress={signOut} />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({});
